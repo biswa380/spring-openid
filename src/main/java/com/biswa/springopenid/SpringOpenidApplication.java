80d1a76b0afa861982aa1ef6b1dc10f4b399700f
Biswabhusan Pradhan <biswabhusanpradhan380@gmail.com>
2018-05-08 19:20:24 +0530

    second commit

---
+++ src/main/java/com/biswa/springopenid/SpringOpenidApplication.java
@@ -0,0 +1,12 @@
+package com.biswa.springopenid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOpenidApplication.class, args);
+	}
+}
